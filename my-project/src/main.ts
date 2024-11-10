0bf92637fc517672f0a509496c3a441b42d0bd18
zeni2492 <mr.sher123@mail.ru>
2024-11-10 15:30:29 +0700

    first

--- my-project/src/main.ts
+++ my-project/src/main.ts
@@ -6,14 +6,7 @@ import {initializeApp} from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBlJdYOU6djsGoFnFCKexu3g9sgaG0RtD0",
-    authDomain: "chat-670cd.firebaseapp.com",
-    projectId: "chat-670cd",
-    storageBucket: "chat-670cd.firebasestorage.app",
-    messagingSenderId: "621267331718",
-    appId: "1:621267331718:web:98964a92f21203bfd63f07",
-    measurementId: "G-W0ZQYEFKRK",
-    databaseURL : "https://chat-670cd-default-rtdb.firebaseio.com/"
+    
 }
 
 const firebase = initializeApp(firebaseConfig)
